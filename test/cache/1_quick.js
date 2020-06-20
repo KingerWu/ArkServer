b213d47e6b488372714e399fb69a442bc9525357
whr <kinger.wu@gmail.com>
2020-06-20 17:49:28 +0800

    增加cache-redis的验证代码

---
+++ test/cache/1_quick.js
@@ -0,0 +1,22 @@
+// 连接redis数据库
+var redis = require('redis');
+var client = redis.createClient({
+    host: "127.0.0.1",
+    port: 6379,
+    password: "123456"
+});
+
+client.on('connect', function () {
+    console.log('connect');
+});
+
+client.on('error', function (err) {
+    console.log('error event - ' + client.host + ':' + client.port + ' - ' + err);
+});
+
+client.set("string key", "string value", redis.print);
+client.get("string key", redis.print)
+
+client.quit(function (err, res) {
+    console.log('Exiting from quit command.');
+});
